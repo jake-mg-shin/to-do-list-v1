f3bc56c68f5b624def7855d61711472276ca2d92
jake-mg-shin <60331338+jake-mg-shin@users.noreply.github.com>
2020-03-24 20:04:01 -0600

    css prefix

--- assets/bg.js
+++ assets/bg.js
@@ -7,10 +7,6 @@ const numbersOfImg = 4;
 bgImg = imgNum => {
   bg.setAttribute("style", `background-image: url("src/images/${imgNum}.jpg")`);
   bg.classList.add("bgImg");
-  // const img = new Image();
-  // img.src = `src/images/${imgNum}.jpg`;
-  // img.classList.add("bgImg");
-  // body.appendChild(img);
 };
 
 // make a random number among 1, 2, 3 and 4
